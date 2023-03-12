1c7140491c4ce735dd15ffaff204f1f154461e2a
劃破黑夜 <179971b@gmail.com>
2023-03-12 18:07:21 +0800

    Add files via upload

--- FairMOT/data_to_pickle.py
+++ FairMOT/data_to_pickle.py
@@ -79,47 +79,6 @@ new_df = pd.DataFrame({
         }, ignore_index=True)
 print( all_id[0].info())
 '''
-def all_id_converter_to_ynet_deprecated(all_id):
-    all_id = [j for g in all_id for j in g]
-    
-    with tqdm(total=len(all_id), desc='Progress') as pbar:
-        for ind, g in enumerate(all_id):
-            last = g.iloc[-1, 0]
-            id = g.iloc[-1, 1]
-            forms = "00000"
-            scenes = [(forms[:-1 * len(str(last))] + str(last)) for g in range(8)]
-            all_id[ind]['sceneId'] = scenes
-            
-            ind_list = []
-            for j in range(1,13):
-                ind_list.append({'frame_n': last + j,
-                                'id' : id,
-                                'x' : 0,
-                                'y' : 0,
-                                'sceneId': (forms[:-1 * len(str(last))] + str(last))
-                                })
-            # Very inefficient, and will spent a lot of time.
-            all_id[ind] = pd.concat([all_id[ind], pd.DataFrame.from_records(ind_list)], ignore_index=True)
-            
-            merge_ = all_id[0]
-            for ind in range(1, len(all_id)):
-                merge_ = pd.concat([merge_, all_id[ind]], ignore_index=True)
-            merge_['frame'] = merge_['frame_n']
-            merge_['metaId'] = merge_['id']
-            merge_['metaId'] = 0
-            merge_['trackId'] = merge_['id']
-            merge_['trackId'] = merge_['trackId'].astype(float)
-            merge_.pop('frame_n')
-            merge_.pop('id')
-            merge_ = merge_.iloc[:, [3, 5, 0, 1, 2, 4]]
-            FairYnet_test_path = '../../ynet/data/FairYnet/'
-            merge_.to_pickle(f'{FairYnet_test_path}fair-mot_test_orig.pkl')
-            
-            pbar.update(1)
-    
-    print('success (2/2)')
-
-# Optimize
 def all_id_converter_to_ynet(all_id):
     all_id = [j for g in all_id for j in g]
     
@@ -181,19 +140,7 @@ if __name__ == "__main__":
         shutil.rmtree("frame_ynet", ignore_errors=True)
     shutil.copytree("frame", "frame_ynet", symlinks=False, ignore=None, copy_function=shutil.copy2, ignore_dangling_symlinks=False, dirs_exist_ok=False)
     
-    import time
-    t1 = time.time()
-    convertion_csv()
-    all_id, uniq_frame = process_data("results.csv")
-    move_frame_FairYnet_ynet(uniq_frame)
-    all_id_converter_to_ynet_deprecated(all_id)
-    t2 = time.time()
-    print("Original speed:", round(t2-t1,4), "s")
-    
-    t1 = time.time()
     convertion_csv()
     all_id, uniq_frame = process_data("results.csv")
     move_frame_FairYnet_ynet(uniq_frame)
     all_id_converter_to_ynet(all_id)
-    t2 = time.time()
-    print("Now speed:", round(t2-t1,4), "s")
